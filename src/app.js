09e4cfddf4e38009d2ce224cfeaec37ac831a71e
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-01-24 16:07:05 +0100

    all setup configured

--- src/app.js
+++ src/app.js
@@ -30,3 +30,11 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 app.use(cookieParser());
+
+//routes
+import router from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", router);
+
+export { app };
